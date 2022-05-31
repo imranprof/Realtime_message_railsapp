543b9b9d46d4918110efe32e6f89bd8a57eab7be
imranprof <imran.ent320@gmail.com>
2022-05-31 21:41:28 +0600

    add redis to gem

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'hirb'
+gem 'redis', '~> 4.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_cable.allowed_request_origins = ['https://imr-chatroom.herokuapp.com/']
+  config.action_cable.allowed_request_origins = ['https://imr-chatroom.herokuapp.com']
 
 end
